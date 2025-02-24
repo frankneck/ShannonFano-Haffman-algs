30ee21e7ec2826c1695cf93da757c98b58bcc09d
frankneck13 <finn.f1rst128@gmail.com>
2025-02-24 14:53:03 +1000

    Added assemblyInfo

--- ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9bac1a3a2c8648778d1be9e911d836e76bad8f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ee3212c40393a4f592544aee820d15a929d41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
